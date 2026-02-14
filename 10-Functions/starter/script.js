64bf133ae82f98935132c950dce651dc5e960118
Sarovan <44712196+Sarovan@users.noreply.github.com>
2026-02-14 00:55:33 -0500

    starting arrays

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -251,38 +251,38 @@ GOOD LUCK 😀
 
 */
 
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  // This generates [0, 0, 0, 0]. More in the next section 😃
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    const answer = Number(
-      prompt(`What is your favourite programming language?
-        0: JavaScript
-        1: Python
-        2: Rust
-        3: C++
-        (Write option number)`),
-    );
-    if (answer >= 0 && answer <= 3) {
-      this.answers[answer]++;
-    }
-    this.displayResults(this.answers, 'string');
-  },
-  displayResults(results, type = 'array') {
-    if (type === 'array') console.log(results);
-    else if (type === 'string')
-      console.log(`Poll results are ${results.join(', ')}`);
-  },
-};
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(`What is your favourite programming language?
+//         0: JavaScript
+//         1: Python
+//         2: Rust
+//         3: C++
+//         (Write option number)`),
+//     );
+//     if (answer >= 0 && answer <= 3) {
+//       this.answers[answer]++;
+//     }
+//     this.displayResults(this.answers, 'string');
+//   },
+//   displayResults(results, type = 'array') {
+//     if (type === 'array') console.log(results);
+//     else if (type === 'string')
+//       console.log(`Poll results are ${results.join(', ')}`);
+//   },
+// };
 
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-poll.displayResults([5, 2, 3], 'string');
-poll.displayResults([1, 5, 3, 9, 6, 1], 'string');
+// poll.displayResults([5, 2, 3], 'string');
+// poll.displayResults([1, 5, 3, 9, 6, 1], 'string');
 
 // const poll = {
 //   question: 'What is your favourite programming language?',
